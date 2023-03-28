c51427b9c760ee53739ca648e0d9cc67712acf8c
imane <imaneaitsitahar@gmail.com>
2023-03-27 19:40:25 -0700

    101-natural.c

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -1,21 +1,25 @@
 #include "main.h"
-
 /**
-* print_rev -> printing a string in reverse
-* @s: the string to be printed in rev
-*/
-
+ * print_rev - imprime en reversa
+ * @s: string
+ * return: 0
+ */
 void print_rev(char *s)
 {
-	int i, n;
+	int var = 0;
+	int o;
 
-	n = 0;
-	while (s[n] != '\0')
-		n++;
-
-	for (i = n - 1; i >= 0; i--)
+	while (*s != '\0')
 	{
-		_putchar(s[i]);
+		var++;
+		s++;
 	}
+	s--;
+	for (o = var ; o > 0; o--)
+	{
+		_putchar(*s);
+		s--;
+	}
+
 	_putchar('\n');
 }
